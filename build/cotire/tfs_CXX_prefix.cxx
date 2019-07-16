cf9a8eef7c898b13dd380a8b6202bde09851b2ac
JaksonSouza <jakson.rts@hotmail.com>
2019-07-15 22:23:17 -0300

    rm build

--- build/cotire/tfs_CXX_prefix.cxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.6 generated file */
-/* /mnt/c/Users/Jakson/Desktop/servidor/build/cotire/tfs_CXX_prefix.cxx */
-#ifdef __cplusplus
-#include "/mnt/c/Users/Jakson/Desktop/servidor/src/otpch.h"
-#endif
--- build/cotire/tfs_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.6 generated file */
-/* /mnt/c/Users/Jakson/Desktop/servidor/build/cotire/tfs_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/mnt/c/Users/Jakson/Desktop/servidor/build/cotire/tfs_CXX_prefix.cxx"
-#endif
